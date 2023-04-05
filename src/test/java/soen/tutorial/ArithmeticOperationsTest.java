5b191a412217e482f87a1915c8e582452cc38b74
z-bruce <87652520+z-bruce@users.noreply.github.com>
2023-04-05 12:41:01 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -17,6 +17,7 @@ public class ArithmeticOperationsTest {
 		assertEquals(expected, actual);
 
 	}
+/*
     @Test
     public void testSub()
     {
@@ -27,5 +28,5 @@ public class ArithmeticOperationsTest {
         assertEquals(expected, actual);
 
     }
-
+*/
 }
